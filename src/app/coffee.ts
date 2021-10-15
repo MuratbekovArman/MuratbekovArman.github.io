4f871c70756f0bb1ad648c475d9b5a77c75a2cd8
nazrakhmetaikun <62601592+nazrakhmetaikun@users.noreply.github.com>
2021-10-15 20:12:14 +0600

    dkjn
    some products added

--- src/app/coffee.ts
+++ src/app/coffee.ts
@@ -28,5 +28,26 @@ export const COFFEE = [
     rating: 4.6,
     amount: 0,
     total: 0
-  }
+  },
+  {
+    id: 4,
+    name: 'Four Sigmatic Mushroom Ground Coffee',
+    price: 14.47,
+    description: 'POWER ON CREATIVE FOCUS: Half the caffeine, more mental clarity; replace your regular beans with ground 100% organic Arabica coffee beans & 500mg of organic Lion’s Mane + Chaga mushroom powder',
+    image: 'https://m.media-amazon.com/images/I/61s0F-yCdAL._SL1500_.jpg',
+    rating: 4.3,
+    amount: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    name: 'Starbucks Breakfast Blend Medium Roast Ground Coffee',
+    price: 20.39,
+    description: 'Breakfast Blend is a lively and lighter roast with a crisp finish.Medium-roasted coffees are smooth and balanced with rich, approachable flavors.',
+    image: 'https://m.media-amazon.com/images/I/81A-5ISp21L._SL1500_.jpg',
+    rating: 4.9,
+    amount: 0,
+    total: 0
+  },
+
 ];
